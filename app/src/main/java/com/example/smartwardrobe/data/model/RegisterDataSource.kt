2dbf9675fbcd191c3fb078672a7eb18d9046157c
shaonwei <sharon.weiss@live.biu.ac.il>
2023-05-10 15:01:17 +0300

    shared preference+closet

--- app/src/main/java/com/example/smartwardrobe/data/model/RegisterDataSource.kt
+++ app/src/main/java/com/example/smartwardrobe/data/model/RegisterDataSource.kt
@@ -1,18 +1,14 @@
 package com.example.smartwardrobe.data.model
 
-import android.content.ContentValues
-import android.util.Log
+
 import com.example.smartwardrobe.RetrofitClient
 import com.example.smartwardrobe.data.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
 import java.io.IOException
 import java.lang.NullPointerException
-import kotlin.coroutines.resume
-import kotlin.coroutines.suspendCoroutine
+
 
 class RegisterDataSource {
     suspend fun register(username: String, mail: String, password: String): Result<LoggedInUser> {
