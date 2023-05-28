ba44e0ae338791cf90e052cc1ada34edde29f39c
shaonwei <sharon.weiss@live.biu.ac.il>
2023-05-28 22:55:27 +0300

    logout

--- app/src/main/java/com/example/smartwardrobe/SettingsFragment.kt
+++ app/src/main/java/com/example/smartwardrobe/SettingsFragment.kt
@@ -1,11 +1,29 @@
 package com.example.smartwardrobe
 
+import android.content.Context
 import android.os.Bundle
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    private lateinit var buttonPreference: Preference
+
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+
+        buttonPreference = findPreference("logoutButton")!!
+
+        // Enable the preference button
+        buttonPreference.isEnabled = true
+        buttonPreference?.setOnPreferenceClickListener {
+            val sharedPreferences = (activity as MainActivity).getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+            if (sharedPreferences.contains("name")) {
+                sharedPreferences.edit().clear().apply()
+                activity?.finish()
+            }
+            true
+        }
     }
 }
